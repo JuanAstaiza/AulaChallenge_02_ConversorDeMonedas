7bd6bbef95832764dcaff77a2201b2a124ac8d32
JuanAstaiza <juancarlosastaiza@gmail.com>
2024-04-16 13:12:21 -0500

    Desafio terminado, se realizo las siguientes actividades: 1. Consumo de la API; 2. Análisis de la Respuesta JSON; 3. Filtro de Monedas; 4. Exibición de Resultados a los usuarios;

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -12,8 +12,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
